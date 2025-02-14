f6adb796a6b4fbcc3bd7f834fe730edf70f7d312
ryanvanle <ryanle4321@gmail.com>
2025-02-13 19:51:48 -0800

    add color cards generation and formatting

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 "use strict";
 
+// disclaimer: AI was only used to do rapid prototyping in speeding of the process writing code that I (ryan) already knew how to implement but would be tedious to do, i.e. component generation.
+
 (function() {
+
+  const COLORS = {
+    "Crimson": "#ad1c42",
+    "Golden Yellow": "#cb8f16",
+    "Emerald": "#009877",
+    "Navy Blue": "#403f6f",
+    "Lavender": "#b2a4d4",
+    "Burnt Orange": "#c7632c",
+    "Charcoal Gray": "#6a6a6a",
+    "Blue Turquoise": "#52b0ae",
+    "Crystal Rose": "#fdc4c7"
+  }
 
   window.addEventListener("load", init);
 
@@ -16,9 +30,8 @@
         button.addEventListener("click", () => showPage("home-page"));
     });
 
+    generateColors();
   }
-
-
 
   /**
    * Shows the specified page and hides all other pages.
@@ -32,6 +45,38 @@
     });
 
     id(pageId).classList.remove("hidden");
+  }
+
+  function generateColors() {
+    generateColorCards();
+    // generateColorDetailPage() // ignore
+  }
+
+  function generateColorCards() {
+    const colorsList = id('colors-list');
+    colorsList.innerHTML = '';
+
+    for (const colorName in COLORS) {
+      if (COLORS.hasOwnProperty(colorName)) {
+        const colorValue = COLORS[colorName];
+        const listItem = document.createElement('li');
+        const article = document.createElement('article');
+        const h2 = document.createElement('h2');
+
+        h2.textContent = colorName;
+
+        const colorSwatch = document.createElement('span');
+        colorSwatch.classList.add('color-swatch');
+        colorSwatch.style.backgroundColor = colorValue;
+
+        article.classList.add("selectable");
+
+        article.appendChild(h2);
+        article.appendChild(colorSwatch);
+        listItem.appendChild(article);
+        colorsList.appendChild(listItem);
+      }
+    }
   }
 
   /**
