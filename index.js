d8c4d3d9bbefa6915bc95e87db46016065cd2fd4
ryanvanle <ryanle4321@gmail.com>
2025-02-13 18:42:30 -0800

    put basic pages formatting down and page navigation

--- index.js
+++ index.js
@@ -5,7 +5,33 @@
   window.addEventListener("load", init);
 
   function init() {
-    console.log("hello world");
+
+    id("materials-button").addEventListener("click", () => showPage("material-page"));
+    id("textures-button").addEventListener("click", () => showPage("texture-page"));
+    id("colors-button").addEventListener("click", () => showPage("colors-page"));
+    id("favorites-button").addEventListener("click", () => showPage("favorite-page"));
+
+    const backButtons = document.querySelectorAll(".back-button");
+    backButtons.forEach(button => {
+        button.addEventListener("click", () => showPage("home-page"));
+    });
+
+  }
+
+
+
+  /**
+   * Shows the specified page and hides all other pages.
+   * @param {string} pageId - The ID of the page to show.
+   */
+  function showPage(pageId) {
+    const pages = document.querySelectorAll(".page");
+
+    pages.forEach(page => {
+      page.classList.add("hidden");
+    });
+
+    id(pageId).classList.remove("hidden");
   }
 
   /**
